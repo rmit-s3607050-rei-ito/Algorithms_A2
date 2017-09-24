e363e219a3dc06d30cebcc49c25f451fdbe41007
rmit-s3607050-rei-ito <s3607050@student.rmit.edu.au>
2017-09-24 17:20:57 +1000

    made original have fixed whitespace also

--- original/mazeGenerator/GrowingTreeGenerator.java
+++ original/mazeGenerator/GrowingTreeGenerator.java
@@ -6,14 +6,14 @@ import maze.Maze;
 import maze.Cell;
 
 public class GrowingTreeGenerator implements MazeGenerator {
-	// Growing tree maze generator. As it is very general, here we implement as "usually pick the most recent cell, but occasionally pick a random cell"
-	
-	double threshold = 0.1;
-	
-	@Override
-	public void generateMaze(Maze maze) {
+  // Growing tree maze generator. As it is very general, here we implement as "usually pick the most recent cell, but occasionally pick a random cell"
+
+  double threshold = 0.1;
+
+  @Override
+  public void generateMaze(Maze maze) {
 
 
-	}
+  }
 
 }
