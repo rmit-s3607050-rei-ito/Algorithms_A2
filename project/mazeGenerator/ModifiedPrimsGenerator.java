731efb1420949dbd13f7dfab9f7498f20712840a
rmit-s3607050-rei-ito <s3607050@student.rmit.edu.au>
2017-10-18 15:12:13 +1100

    fixed gitignore

--- project/mazeGenerator/ModifiedPrimsGenerator.java
+++ project/mazeGenerator/ModifiedPrimsGenerator.java
@@ -17,7 +17,6 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
     List<Cell> cellList = new ArrayList<Cell>();
     List<Cell> frontier = new ArrayList<Cell>();
     // 1. Pick random starting cell
-    //Cell start = maze.map[rand.nextInt(maze.sizeR)][rand.nextInt(maze.sizeC)];
     Cell start = randomizeStartCell(maze);
 
     // Start loop here
@@ -25,61 +24,34 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
     cellList.add(start);
     in[start.r][start.c] = true;
 
-    // System.out.println("Starting at: " + start.r + " , " + start.c);
     // 1. Put all neighbouring cells of starting cell into frontier
     addNeighbours(maze, start, in, frontier);
     int totalCells = maze.sizeR * maze.sizeC;
 
     while (cellList.size() != totalCells) {
-    // for (int i = 0; i < 5; i++) {
       // 2. Randomly select cell from frontier set
       Cell selected = frontier.get(rand.nextInt(frontier.size()));
-      // System.out.println("Start with (removed from frontier): " + selected.r + " , " + selected.c);
       // 2. Remove it from frontier set
       frontier.remove(selected);
       // 2. Randomly select another cell 'closest' that is in the cellList that is adjacent to selected cell
       Cell closest = getClosestNeighbour(maze, cellList, selected);
-      // System.out.println("Random closest selected from cellList: " + closest.r + " , " + closest.c);
 
       // 2. Get direction between selected cell and the closest
       int dir = getDirection(maze.type, selected, closest);
-      // System.out.println("Carved in direction: " + dir);
       // 2. Carve a path
       maze.map[selected.r][selected.c].wall[dir].present = false;
       maze.map[selected.r][selected.c].wall[dir].drawn = false;
-      // System.out.println("direction = " + dir);
 
       // 3. Add selected cell to set cellList
-      // System.out.println("Added to cellList: " + selected.r + "," + selected.c);
       cellList.add(selected);
       in[selected.r][selected.c] = true;
-      // start = selected;
-
-      // System.out.println("In Frontier (Before add neighbours): ");
-      // for (int i = 0; i < frontier.size(); i++) {
-      //   System.out.println(" - At: " + frontier.get(i).r + " , " + frontier.get(i).c);
-      // }
 
       // 3. Add neighbours of selected cell to the frontier
       addNeighbours(maze, selected, in, frontier);   // Remove
 
-      // System.out.println("In Frontier (After add neighbour): ");
-      // for (int i = 0; i < frontier.size(); i++) {
-      //   System.out.println(" - At: " + frontier.get(i).r + " , " + frontier.get(i).c);
-      // }
-
       // [DEBUG] - Uncomment to view how many cells left to process
       // System.out.println("Cells to process = " + cellList.size());
     }
-
-    //  System.out.println("Remove selected");
-    //  for (int i = 0; i < frontier.size(); i++) {
-    //    System.out.println("Neighbour at: " + frontier.get(i).r + " , " + frontier.get(i).c);
-    //  }
-
-
-    //  if (maze.type == Maze.NORMAL)
-    //    generateNormalMaze(maze, start, in, frontier);
   } // end of generateMaze()
 
   private Cell randomizeStartCell(Maze maze) {
@@ -150,12 +122,6 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
     Random rand = new Random();
     closest = closestNeighbours.get(rand.nextInt(closestNeighbours.size()));
 
-    // System.out.println("Neighbours in cellSet: ");
-    // for (Cell c : closestNeighbours) {
-    //   System.out.println(" - At: " + c.r + " , " + c.c);
-    // }
-    // System.out.println();
-
     return closest;
   }
 
@@ -192,120 +158,5 @@ public class ModifiedPrimsGenerator implements MazeGenerator {
 
     // Default just return east, so its not checked
     return Maze.EAST;
-
-    //if (start.r < end.r)
-    //  return Maze.NORTH;
-    //else if (start.r > end.r)
-    //  return Maze.SOUTH;
-    //else if (start.c > end.c)
-    //  return Maze.WEST;
-    //else
-    //  return Maze.EAST;
   }
-
-  // @Override
-  // public void generateMaze(Maze maze) {
-  //   Random rand = new Random();
-  //   boolean in[][] = new boolean[maze.sizeR][maze.sizeC];
-  //   List<Cell> frontier = new ArrayList<Cell>();
-  //   //Cell start = maze.map[rand.nextInt(maze.sizeR)][rand.nextInt(maze.sizeC)];
-  //   Cell start = maze.map[2][0];
-  //
-  //   if (maze.type == Maze.NORMAL)
-  //     generateNormalMaze(maze, start, in, frontier);
-  // } // end of generateMaze()
-  //
-  // // Select random starting position
-  // private Cell createRandomStart(int maxRow, int maxCol) {
-  //   Random rand = new Random();
-  //   int min = 0, row, col;
-  //
-  //   row = rand.nextInt(maxRow) + min;
-  //   col = rand.nextInt(maxCol) + min;
-  //
-  //   Cell starting = new Cell(row, col);
-  //
-  //   return starting;
-  // }
-  //
-  // private void addToFrontier(Cell cell, boolean[][]in, List<Cell> front) {
-  //   int xMax = in.length;
-  //   int yMax = in[0].length;
-  //
-  //   if (inBounds(cell, xMax, yMax) && !in[cell.r][cell.c]) {
-  //     front.add(cell);
-  //   }
-  // }
-  //
-  // private boolean inBounds(Cell cell, int rowMax, int colMax) {
-  //   // Check to ensure coordinates are inside the maze
-  //   if (cell.r < 0 || cell.c < 0 || cell.r >= rowMax || cell.c >= colMax)
-  //     return false;
-  //
-  //   return true;
-  // }
-  //
-  // private void mark(Cell cell, boolean[][]in, List<Cell> front) {
-  //   in[cell.r][cell.c] = true;
-  //
-  //   if (cell.neigh[Maze.NORTH] != null)
-  //     addToFrontier(cell.neigh[Maze.NORTH], in, front);
-  //   if (cell.neigh[Maze.SOUTH] != null)
-  //     addToFrontier(cell.neigh[Maze.SOUTH], in, front);
-  //   if (cell.neigh[Maze.WEST] != null)
-  //     addToFrontier(cell.neigh[Maze.WEST], in, front);
-  //   if (cell.neigh[Maze.EAST] != null)
-  //     addToFrontier(cell.neigh[Maze.EAST], in, front);
-  // }
-  //
-  // private void generateNormalMaze(Maze maze, Cell start, boolean[][]in, List<Cell> front) {
-  //   Random rand = new Random();
-  //   Cell currCell = start;
-  //   mark(maze.map[start.r][start.c], in, front);
-  //
-  //   int step = 4;
-  //
-  //   while (!front.isEmpty()) {
-  //   //while (step > 0 ) {
-  //     System.out.println("Frontier List: ");
-  //     for (Cell p : front)
-  //       System.out.println("x: " + p.r + " y: " + p.c);
-  //     System.out.println("Start: " + currCell.r + ", " + currCell.c);
-  //
-  //     front.remove(currCell);
-  //     mark(currCell, in, front);
-  //
-  //     Cell randFront = front.get(rand.nextInt(front.size()));
-  //     //while (randFront.r == currCell.r && randFront.c == currCell.c) {
-  //     //  //if (!front.isEmpty())
-  //     //  randFront = front.get(rand.nextInt(front.size()));
-  //     //}
-  //
-  //     System.out.println("Front: " + randFront.r + ", " + randFront.c);
-  //
-  //     currCell = randFront;
-  //
-  //     ArrayList<Cell> inNeigh = new ArrayList<Cell>();
-  //     for (int i = 0; i < randFront.neigh.length; i++) {
-  //       if (randFront.neigh[i] != null) {
-  //         if (in[randFront.neigh[i].r][randFront.neigh[i].c]) {
-  //           inNeigh.add(randFront.neigh[i]);
-  //         }
-  //       }
-  //     }
-  //
-  //     System.out.println("In List: ");
-  //     for (Cell p : inNeigh)
-  //       System.out.println("x: " + p.r + " y: " + p.c);
-  //
-  //     Cell randNeigh = inNeigh.get(rand.nextInt(inNeigh.size()));
-  //     int dir = direction(randFront, randNeigh);
-  //     maze.map[randFront.r][randFront.c].wall[dir].present = false;
-  //     maze.map[randFront.r][randFront.c].wall[dir].drawn = false;
-  //     System.out.println("direction = " + dir);
-  //
-  //     step--;
-  //   }
-  // }
-  //
 } // end of class ModifiedPrimsGenerator
